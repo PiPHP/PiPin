d034f3d0bef5895603ef86d52cef8710078575db
Andrew Carter <andrewcarter1992@gmail.com>
2015-12-13 19:10:17 +0000

    Bugfix in value command

--- src/ValueCommand.php
+++ src/ValueCommand.php
@@ -2,6 +2,7 @@
 
 namespace PiPHP\PiPin;
 
+use PiPHP\GPIO\PinInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
